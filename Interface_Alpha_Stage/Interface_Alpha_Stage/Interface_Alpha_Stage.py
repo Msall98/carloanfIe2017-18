144505c8eebc828d987fd2b79d900d9ec1b788b8
Sim Shih Heng <efyss4@nottingham.edu.my>
2018-03-15 13:40:14 +0800

    Add a two button with Kivy framework with the return of "Hello,World" in the command window and a kill switch.

--- Interface_Alpha_Stage/Interface_Alpha_Stage/Interface_Alpha_Stage.py
+++ Interface_Alpha_Stage/Interface_Alpha_Stage/Interface_Alpha_Stage.py
@@ -1,1 +1,39 @@
-print("Testing")
+import sys
+import kivy
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+kivy.require('1.0.6') # replace with your current kivy version !
+
+
+class carloangui(BoxLayout):
+    pass
+
+    def __init__(self, **kwargs):
+        super(carloangui,self).__init__(**kwargs)
+        print('The button <%s> is being pressed')
+
+        btn1 = Button(text='Hello world 1',
+                      background_color=(0, 0, 1, 1))
+        btn1.bind(on_press=self.clk)
+        self.add_widget(btn1)
+        btn2 = Button(text='Kill')
+        btn2.bind(on_press=self.quit)
+        self.add_widget(btn2)
+    def clk(self,obj):
+        print("Hello,World")
+    def quit(self,obj):
+        sys.exit()
+
+
+
+class open(App):
+    def build(self):
+        cl=carloangui()
+        return cl
+
+if __name__ == '__main__':
+    open().run()
+print("***************************************************************** \n" + " \n\n                         Car Loan Calculator                               ")
+print("\n \n \n*****************************************************************")
