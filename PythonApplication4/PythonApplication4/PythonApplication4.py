2a4065c0362a2b263a22ae16475cd787ec3df499
Sim Shih Heng <efyss4@nottingham.edu.my>
2018-03-28 00:05:57 +0800

    Minor improvement on the interface. Fix the size of the optionmenu. Done some updates on the labels.

--- PythonApplication4/PythonApplication4/PythonApplication4.py
+++ PythonApplication4/PythonApplication4/PythonApplication4.py
@@ -9,10 +9,10 @@ class LoanCalculator:
     def __init__(self):
         self.window = Tk() # Create a self.window
         self.window.title("Car Loan Calculator") # Set title
-        self.window.geometry('500x250')
+        self.window.geometry('275x150')
         self.window.resizable(False,False)
         # Make some labels in grid form.
-        Label(self.window, text = "Vehicle Cost      (In RM) :").grid(row = 1, 
+        Label(self.window, text = "Vehicle Cost (In RM) :").grid(row = 1, 
             column = 1, sticky = W)
         Label(self.window, text = "Down Payment (In RM) :").grid(row = 2, 
             column = 1, sticky = W)
@@ -34,7 +34,7 @@ class LoanCalculator:
         self.carloan = StringVar()
         self.carloan.set(self.carloantimelist[0])
         self.carloanomenu=OptionMenu(self.window,self.carloan,*self.carloantimelist)
-        self.carloanomenu.grid(row=3,column=2)
+        self.carloanomenu.grid(row=3,column=2,sticky="ew")
 
         self.interest = StringVar()
         Entry(self.window, textvariable = self.interest, 
@@ -64,10 +64,10 @@ class LoanCalculator:
             self.window.withdraw()
             self.popup=tk.Tk()
             self.popup.wm_title("Error")
-            self.popup.geometry("550x200")
+            self.popup.geometry("350x100")
             self.popup.resizable(False,False)
             label = ttk.Label(self.popup,text="Missing Information")
-            label.config(font=("Arial",12))
+            label.config(font=("Arial",10))
             label.pack(pady=10,anchor=CENTER)
             B1 = ttk.Button(self.popup,text="OK",command = self.fixwindow)
             B1.pack()
@@ -78,7 +78,7 @@ class LoanCalculator:
             self.window.withdraw()
             self.popup=tk.Tk()
             self.popup.wm_title("Invaild Information")
-            self.popup.geometry("550x200")
+            self.popup.geometry("350x100")
             self.popup.resizable(False,False)
             label = ttk.Label(self.popup,text="Invalid information entered")
             label.config(font=("Arial",12))
@@ -105,9 +105,10 @@ def algorithm(self,price,down_payment,time,interest_rate):
         self.window.withdraw()
         self.popup=tk.Tk()
         self.popup.wm_title("Thank you")
-        self.popup.geometry("500x200")
+        self.popup.geometry("400x125")
         self.popup.resizable(False,False)
-        label_1 = ttk.Label(self.popup,text=('Your monthly Installment for loaning RM '+str(price)+'\nwith a down payment of RM '+str(down_payment)+' for '+str(time)+' year(s) is:\n RM '+"{0:.2f}".format(Monthly_installment)))
+        label_1 = ttk.Label(self.popup,text=('Your monthly Installment for loaning RM '+str(price)+'\n with a down payment of RM '+str(down_payment)+
+                                             ' for '+str(time)+' year(s) is: \n RM '+"{0:.2f}".format(Monthly_installment) + " Records have been made and save into 'loan.txt' "))
         label_1.pack(pady=10,anchor=CENTER)
         B1 = ttk.Button(self.popup,text="OK",command = self.fixwindow)
         B1.pack()
@@ -117,7 +118,7 @@ def algorithm(self,price,down_payment,time,interest_rate):
         self.window.withdraw()
         self.popup=tk.Tk()
         self.popup.wm_title("Error")
-        self.popup.geometry("550x200")
+        self.popup.geometry("350x100")
         self.popup.resizable(False,False)
         label = ttk.Label(self.popup,text=('Down Payment should not be more than the loan amount'))
         label.pack(pady=10,anchor=CENTER)
