1e10d41bf36f8490f6ce3ff3691e2dfca6364fef
Sim Shih Heng <efyss4@nottingham.edu.my>
2018-03-27 23:13:26 +0800

    Added a function when a popup window appears, the main program menu will hide itself untill the "OK" button is pressed. "popup: variable have changed to "self.popup" .

--- PythonApplication4/PythonApplication4/PythonApplication4.py
+++ PythonApplication4/PythonApplication4/PythonApplication4.py
@@ -43,7 +43,11 @@ class LoanCalculator:
         btComputePayment = Button(self.window, text = "Compute Payment", 
             command = self.maininput).grid(
                 row = 9, column = 2)
-
+        #Remainder: No matter what, in Tkinter you must have the event loop or the program will close itself.
+        self.window.mainloop()
+    def fixwindow(self):
+        self.popup.destroy()
+        self.window.deiconify()
     def maininput(self):
         #To Combine Two Function as One.
         price1=self.price.get()
@@ -53,58 +57,63 @@ class LoanCalculator:
         time=int(self.carloan.get())
         interest_rate=self.interest.get()
         if price.replace('.','',1).isdigit()==True and interest_rate.replace('.','',1).isdigit()==True and down_payment.replace('.','',1).isdigit()==True:
-            algorithm(price,down_payment,time,interest_rate)           
+            algorithm(self,price,down_payment,time,interest_rate)           
 
 
         elif price=='' or down_payment=='' or interest_rate=='':
-            popup=tk.Tk()
-            popup.wm_title("Error")
-            popup.geometry("550x200")
-            popup.resizable(False,False)
-            label = ttk.Label(popup,text="Missing Information")
+            self.window.withdraw()
+            self.popup=tk.Tk()
+            self.popup.wm_title("Error")
+            self.popup.geometry("550x200")
+            self.popup.resizable(False,False)
+            label = ttk.Label(self.popup,text="Missing Information")
             label.config(font=("Arial",12))
             label.pack(pady=10,anchor=CENTER)
-            B1 = ttk.Button(popup,text="OK",command = popup.destroy)
+            B1 = ttk.Button(self.popup,text="OK",command = self.fixwindow)
             B1.pack()
-            popup.mainloop()
+            
+            self.popup.mainloop()
 
         else:
-                popup=tk.Tk()
-                popup.wm_title("Invaild Information")
-                popup.geometry("550x200")
-                popup.resizable(False,False)
-                label = ttk.Label(popup,text="Invalid information entered")
-                label.config(font=("Arial",12))
-                label.pack(pady=10,anchor=CENTER)
-                B1 = ttk.Button(popup,text="OK",command = popup.destroy)
-                B1.pack()
-                popup.mainloop()            
+            self.window.withdraw()
+            self.popup=tk.Tk()
+            self.popup.wm_title("Invaild Information")
+            self.popup.geometry("550x200")
+            self.popup.resizable(False,False)
+            label = ttk.Label(self.popup,text="Invalid information entered")
+            label.config(font=("Arial",12))
+            label.pack(pady=10,anchor=CENTER)
+            B1 = ttk.Button(self.popup,text="OK",command = self.fixwindow)
+            B1.pack()
+            self.popup.mainloop()            
 
-def algorithm(price,down_payment,time,interest_rate):
+def algorithm(self,price,down_payment,time,interest_rate):
     if float(price)>=float(down_payment):
         loanamount=float(price)-float(down_payment)
         Total_interest=(float(interest_rate)/100)*time*loanamount
         Monthly_interest=Total_interest/(time*12)
         Monthly_installment=(loanamount+Total_interest)/(time*12)
-
-        popup=tk.Tk()
-        popup.wm_title("Thank you")
-        popup.geometry("500x200")
-        popup.resizable(False,False)
-        label_1 = ttk.Label(popup,text=('Your monthly Installment for loaning RM '+str(price)+'\nwith a down payment of RM '+str(down_payment)+' for '+str(time)+' year(s) is:\n RM '+"{0:.2f}".format(Monthly_installment)))
+        self.window.withdraw()
+        self.popup=tk.Tk()
+        self.popup.wm_title("Thank you")
+        self.popup.geometry("500x200")
+        self.popup.resizable(False,False)
+        label_1 = ttk.Label(self.popup,text=('Your monthly Installment for loaning RM '+str(price)+'\nwith a down payment of RM '+str(down_payment)+' for '+str(time)+' year(s) is:\n RM '+"{0:.2f}".format(Monthly_installment)))
         label_1.pack(pady=10,anchor=CENTER)
-        B1 = ttk.Button(popup,text="OK",command = popup.destroy)
+        B1 = ttk.Button(self.popup,text="OK",command = self.fixwindow)
         B1.pack()
-        popup.mainloop()
+        self.popup.mainloop()
     else:
-        popup=tk.Tk()
-        popup.wm_title("Error")
-        popup.geometry("550x200")
-        popup.resizable(False,False)
-        label = ttk.Label(popup,text=('Down Payment should not be more than the loan amount'))
+        self.window.withdraw()
+        self.popup=tk.Tk()
+        self.popup.wm_title("Error")
+        self.popup.geometry("550x200")
+        self.popup.resizable(False,False)
+        label = ttk.Label(self.popup,text=('Down Payment should not be more than the loan amount'))
         label.pack(pady=10,anchor=CENTER)
-        B1 = ttk.Button(popup,text="OK",command = popup.destroy)
+        B1 = ttk.Button(self.popup,text="OK",command = self.fixwindow)
         B1.pack()
-        popup.mainloop()
+        self.popup.mainloop()
+
 
 LoanCalculator()# Create GUI
